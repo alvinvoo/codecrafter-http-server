35575ba36cd770e1a347c24209f8a1bc1dc28f08
alvin.voo <alvin.voo@gmail.com>
2024-08-30 14:35:27 +0800

    added response for echo

--- app/server.go
+++ app/server.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"os"
 	"strings"
+
+	"github.com/codecrafters-io/http-server-starter-go/app/util"
 )
 
 const (
@@ -47,16 +49,38 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		// TODO: use Scanner for iteration
 		if strings.HasPrefix(requestLine, "GET") {
 			requestTarget := strings.Split(requestLine, " ")[1]
 
 			var response string
 			if requestTarget == "/" {
-				response = "HTTP/1.1 200 OK\r\n\r\n"
+				response = constructResponse(Response{
+					status: Status{
+						statusCode: 200,
+						statusText: "OK",
+					},
+				})
+			} else if strings.HasPrefix(requestTarget, "/echo/") {
+				str := extractPathSegment(requestTarget)
+				response = constructResponse(Response{
+					status: Status{
+						statusCode: 200,
+						statusText: "OK",
+					}, headers: &Headers{
+						contentType: "text/plain",
+						contentLen:  len(str),
+					}, body: str,
+				})
 			} else {
-				response = "HTTP/1.1 404 Not Found\r\n\r\n"
+				response = constructResponse(Response{
+					status: Status{
+						statusCode: 404,
+						statusText: "Not Found",
+					},
+				})
 			}
+
+			util.DebugLog("Response", response)
 			_, err := tcpConn.Write([]byte(response))
 			if err != nil {
 				fmt.Println("Error writing response: ", err.Error())
@@ -67,4 +91,50 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 	}
+}
+
+func extractPathSegment(url string) string {
+	// Split the URL by "/" and get the part after "echo"
+	parts := strings.Split(url, "/")
+	if len(parts) > 2 {
+		return parts[2] // Get the segment after "/echo/"
+	}
+	return ""
+}
+
+type Status struct {
+	statusCode int
+	statusText string
+}
+
+type Headers struct {
+	contentType string
+	contentLen  int
+}
+
+type Response struct {
+	status  Status
+	headers *Headers
+	body    string
+}
+
+func constructResponse(response Response) string {
+	statusLine := fmt.Sprintf("HTTP/1.1 %d %s\r\n", response.status.statusCode, response.status.statusText)
+
+	var (
+		headers string
+		body    string
+	)
+	if response.headers != nil {
+		headers = fmt.Sprintf("Content-Type: %s\r\nContent-Length: %d\r\n\r\n", response.headers.contentType, response.headers.contentLen)
+	}
+	if response.body != "" {
+		body = response.body
+	}
+
+	if response.headers == nil && response.body == "" {
+		statusLine += "\r\n"
+	}
+
+	return fmt.Sprintf("%s%s%s", statusLine, headers, body)
 }
