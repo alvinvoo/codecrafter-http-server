0b8534c68a7650c6d7ed435807aa856d0963c02c
alvin.voo <alvin.voo@gmail.com>
2024-08-30 16:17:43 +0800

    added request handling; user-agent

--- app/server.go
+++ app/server.go
@@ -43,98 +43,56 @@ func handleConnection(conn net.Conn) {
 		tcpConn.SetKeepAlivePeriod(10)
 
 		reader := bufio.NewReader(tcpConn)
-		requestLine, err := reader.ReadString('\n')
+		request, err := unserializeRequest(reader)
 		if err != nil {
 			fmt.Println("Error reading request: ", err.Error())
 			return
 		}
 
-		if strings.HasPrefix(requestLine, "GET") {
-			requestTarget := strings.Split(requestLine, " ")[1]
-
-			var response string
-			if requestTarget == "/" {
-				response = constructResponse(Response{
-					status: Status{
-						statusCode: 200,
-						statusText: "OK",
-					},
-				})
-			} else if strings.HasPrefix(requestTarget, "/echo/") {
-				str := extractPathSegment(requestTarget)
-				response = constructResponse(Response{
-					status: Status{
-						statusCode: 200,
-						statusText: "OK",
-					}, headers: &Headers{
-						contentType: "text/plain",
-						contentLen:  len(str),
-					}, body: str,
-				})
-			} else {
-				response = constructResponse(Response{
-					status: Status{
-						statusCode: 404,
-						statusText: "Not Found",
-					},
-				})
-			}
-
-			util.DebugLog("Response", response)
-			_, err := tcpConn.Write([]byte(response))
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
-			}
+		var response string
+		if request.requestLine.url == "/" {
+			response = serializeResponse(Response{
+				status: Status{
+					statusCode: 200,
+					statusText: "OK",
+				},
+			})
+			// TODO: can extract out the url path segments
+		} else if strings.HasPrefix(request.requestLine.url, "/echo") {
+			str := extractPathSegment(request.requestLine.url)
+			response = serializeResponse(Response{
+				status: Status{
+					statusCode: 200,
+					statusText: "OK",
+				}, headers: &RespHeaders{
+					contentType: "text/plain",
+					contentLen:  len(str),
+				}, body: str,
+			})
+		} else if strings.HasPrefix(request.requestLine.url, "/user-agent") {
+			response = serializeResponse(Response{
+				status: Status{
+					statusCode: 200,
+					statusText: "OK",
+				}, headers: &RespHeaders{
+					contentType: "text/plain",
+					contentLen:  len(request.headers.userAgent),
+				}, body: request.headers.userAgent,
+			})
 		} else {
-			fmt.Println("Unsupported method: ", requestLine)
-			return
+			response = serializeResponse(Response{
+				status: Status{
+					statusCode: 404,
+					statusText: "Not Found",
+				},
+			})
+		}
+
+		util.DebugLog("Response", response)
+		_, err = tcpConn.Write([]byte(response))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+			os.Exit(1)
 		}
 	}
-}
-
-func extractPathSegment(url string) string {
-	// Split the URL by "/" and get the part after "echo"
-	parts := strings.Split(url, "/")
-	if len(parts) > 2 {
-		return parts[2] // Get the segment after "/echo/"
-	}
-	return ""
-}
-
-type Status struct {
-	statusCode int
-	statusText string
-}
-
-type Headers struct {
-	contentType string
-	contentLen  int
-}
-
-type Response struct {
-	status  Status
-	headers *Headers
-	body    string
-}
-
-func constructResponse(response Response) string {
-	statusLine := fmt.Sprintf("HTTP/1.1 %d %s\r\n", response.status.statusCode, response.status.statusText)
-
-	var (
-		headers string
-		body    string
-	)
-	if response.headers != nil {
-		headers = fmt.Sprintf("Content-Type: %s\r\nContent-Length: %d\r\n\r\n", response.headers.contentType, response.headers.contentLen)
-	}
-	if response.body != "" {
-		body = response.body
-	}
-
-	if response.headers == nil && response.body == "" {
-		statusLine += "\r\n"
-	}
-
-	return fmt.Sprintf("%s%s%s", statusLine, headers, body)
 }
